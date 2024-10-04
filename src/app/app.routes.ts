1b05adc5d9f53316995ec7eea51156baeaa6ed7f
webskitters <sayantan.singha@webskitters.in>
2024-10-04 19:44:43 +0530

    canvas 3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,6 @@ export const routes: Routes = [
   { path: '', redirectTo: '/canvas1', pathMatch: 'full' },
   { path: 'canvas1', loadComponent: () => import('./canvas1/canvas1.component').then(c => c.Canvas1Component) },
   { path: 'canvas2', loadComponent: () => import('./canvas2/canvas2.component').then(c => c.Canvas2Component) },
+  { path: 'canvas3', loadComponent: () => import('./canvas3/canvas3.component').then(c => c.Canvas3Component) },
   { path: '**', redirectTo: '/canvas1' }
 ];
