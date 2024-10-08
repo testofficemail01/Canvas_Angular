2b261fa52c9953ff9417fda283f11f0240ffb5b9
webskitters <sayantan.singha@webskitters.in>
2024-10-08 11:26:33 +0530

    canvas 1

--- src/app/canvas1/canvas1.component.ts
+++ src/app/canvas1/canvas1.component.ts
@@ -1,5 +1,5 @@
 import { DOCUMENT, isPlatformBrowser } from '@angular/common';
-import { Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { afterNextRender, Component, ElementRef, inject, OnInit, PLATFORM_ID, signal, viewChild } from '@angular/core';
 
 @Component({
   selector: 'app-canvas1',
@@ -8,99 +8,132 @@ import { Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
   templateUrl: './canvas1.component.html',
   styleUrl: './canvas1.component.scss'
 })
-export class Canvas1Component implements OnInit {
+export class Canvas1Component {
 
   private document = inject(DOCUMENT);
-  private platformId = inject(PLATFORM_ID);
+  private canvasElement = viewChild<ElementRef>('canvas');
+  private isDragging = signal<boolean>(false);
 
-  ngOnInit(): void {
-    if (isPlatformBrowser(this.platformId)) {
-      const canvas: HTMLCanvasElement = this.document.getElementById('canvas1') as HTMLCanvasElement;
+  private circle = { x: 95, y: 150, radius: 30 }; // Circle properties
+  private offset = { x: 0, y: 0 }; // Offset for dragging
 
-      if (canvas) {
-        const ctx = canvas.getContext('2d');
-        const pixelRatio = window.devicePixelRatio || 1;
-        console.log("ctx: ", ctx);
+  constructor() {
+    afterNextRender(() => {
+      const canvasElement = this.canvasElement()
+      if (canvasElement) {
+        const canavs: HTMLCanvasElement = canvasElement.nativeElement;
+        if (canavs) {
+          this.canvasInit(canavs);
 
-        if (ctx) {
-          const width = canvas.clientWidth;
-          const height = canvas.clientHeight;
+          canavs.addEventListener('mousedown', this.onMouseDown.bind(this));
+          canavs.addEventListener('mousemove', this.onMouseMove.bind(this));
+          canavs.addEventListener('mouseup', this.onMouseUp.bind(this));
+          // canavs.addEventListener('mouseleave', this.onMouseUp.bind(this)); // Handle case where mouse leaves the canvas
 
-          // Set the actual canvas size, accounting for pixel ratio
-          canvas.width = width * pixelRatio;
-          canvas.height = height * pixelRatio;
-
-          // Scale the canvas context to accommodate the pixel ratio
-          ctx.scale(pixelRatio, pixelRatio);
-
-          ctx.font = "bold 48px serif";
-
-          /* Draw two lines crossing eatch other and a text at center */
-          // 1st line
-          ctx.beginPath();
-          ctx.moveTo(0, 0);
-          ctx.lineTo(width, height);
-          ctx.lineWidth = 1;
-          ctx.strokeStyle = 'red';
-          ctx.stroke();
-
-          // text
-          ctx.beginPath();
-          ctx.strokeStyle = 'green';
-          ctx.strokeStyle
-          ctx.strokeText('Study', width / 2 - 63, height / 2 + 10);
-
-          // 2nd line
-          ctx.beginPath();
-          ctx.moveTo(width, 0);
-          ctx.lineTo(0, height);
-          ctx.lineWidth = 1;
-          ctx.strokeStyle = 'blue';
-          ctx.stroke();
+        }
+      }
+    })
+  }
 
 
+  private canvasInit(canvas: HTMLCanvasElement): void {
+    const gap = 15;
+    const ctx = canvas.getContext('2d');
+    const { width, height } = canvas.getBoundingClientRect();
+    canvas.width = width;
+    canvas.height = height;
+    const _width = 400;
+    const _height = height - 50;
+    if (ctx) {
+      ctx.strokeStyle = '#000';
+      ctx.lineWidth = 2;
+      ctx.beginPath()
+      ctx.moveTo(gap, gap)
+      ctx.lineTo(gap, _height)
+      ctx.lineTo(_width, _height)
+      ctx.lineTo(_width, gap)
+      ctx.lineTo(gap, gap)
+      ctx.closePath()
+      ctx.stroke();
+      this.drawCircle(ctx)
+    }
+  }
+
+  private drawLinesToEdges(ctx: CanvasRenderingContext2D): void {
+    const canvas = ctx.canvas;
+
+    // edges of the circle
+    const leftEdge = this.circle.x - this.circle.radius;
+    const rightEdge = this.circle.x + this.circle.radius;
+    const topEdge = this.circle.y - this.circle.radius;
+    const bottomEdge = this.circle.y + this.circle.radius;
+
+    ctx.strokeStyle = '#00b7ff';
+    ctx.lineWidth = 2;
+
+    ctx.beginPath();
+    ctx.moveTo(leftEdge, this.circle.y);
+    ctx.lineTo(0, this.circle.y);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(rightEdge, this.circle.y);
+    ctx.lineTo(canvas.width, this.circle.y);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(this.circle.x, topEdge);
+    ctx.lineTo(this.circle.x, 0);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(this.circle.x, bottomEdge);
+    ctx.lineTo(this.circle.x, canvas.height);
+    ctx.stroke();
+  }
 
 
-          /** Draw a circle */
-          ctx.beginPath();
-          ctx.arc(300, 150, 50, 0, 2 * Math.PI);
-          ctx.fillStyle = 'red';
-          ctx.fill();
+  private drawCircle(ctx: CanvasRenderingContext2D): void {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height); // Clear canvas before drawing
+
+    ctx.beginPath();
+    ctx.fillStyle = 'green';
+    ctx.arc(this.circle.x, this.circle.y, this.circle.radius, 0, 2 * Math.PI);
+    ctx.fill();
+
+    this.drawLinesToEdges(ctx);
+  }
 
 
+  onMouseDown(event: MouseEvent) {
+    const { offsetX, offsetY } = event;
+    // Check if the mouse is inside the circle
+    const dx = offsetX - this.circle.x;
+    const dy = offsetY - this.circle.y;
+    if (dx * dx + dy * dy <= this.circle.radius * this.circle.radius) {
+      this.isDragging.set(true);
+      this.offset.x = dx;
+      this.offset.y = dy;
+    }
+  }
+  onMouseMove(event: MouseEvent) {
+    if (this.isDragging()) {
+      const { offsetX, offsetY } = event;
+      // Update circle position
+      this.circle.x = offsetX - this.offset.x;
+      this.circle.y = offsetY - this.offset.y;
 
-
-          /* draw a arch */
-          // Tangential lines
-          ctx.beginPath();
-          ctx.strokeStyle = "gray";
-          ctx.moveTo(200, 20);
-          ctx.lineTo(200, 130);
-          ctx.lineTo(50, 20);
-          ctx.stroke();
-
-          // Arc
-          ctx.beginPath();
-          ctx.strokeStyle = "black";
-          ctx.lineWidth = 5;
-          ctx.moveTo(200, 20);
-          ctx.arcTo(200, 130, 50, 20, 40);
-          ctx.stroke();
-
-          // Start point
-          ctx.beginPath();
-          ctx.fillStyle = "blue";
-          ctx.arc(200, 20, 5, 0, 2 * Math.PI);
-          ctx.fill();
-
-          // Control points
-          ctx.beginPath();
-          ctx.fillStyle = "red";
-          ctx.arc(200, 130, 5, 0, 2 * Math.PI); // Control point one
-          ctx.arc(50, 20, 5, 0, 2 * Math.PI); // Control point two
-          ctx.fill();
+      const canvasElement = this.canvasElement();
+      if (canvasElement) {
+        const canvas: HTMLCanvasElement = canvasElement.nativeElement;
+        if (canvas) {
+          const ctx = canvas.getContext('2d');
+          ctx && this.drawCircle(ctx);
         }
       }
     }
+  }
+  onMouseUp(event: MouseEvent) {
+    this.isDragging.set(false);
   }
 }
